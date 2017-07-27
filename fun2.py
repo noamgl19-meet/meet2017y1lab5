b7c51fcb12d093efdb8b7daefb45c26c7dfdf267
noamgl19-meet <noamgl19@meet.mit.edu>
2017-07-27 13:52:58 +0300

    Stopping here

---
+++ fun2.py
@@ -0,0 +1,22 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: /home/student/noamgl19_lab5/meet2017y1lab5/fun2.py ========
+>>> 
+======== RESTART: /home/student/noamgl19_lab5/meet2017y1lab5/fun2.py ========
+>>> draw_1d(c1, 12)
+~~~~~~~~~~~~
+>>> draw_1d(c2, 12)
+!!!!!!!!!!!!
+>>> 
+======== RESTART: /home/student/noamgl19_lab5/meet2017y1lab5/fun2.py ========
+>>> draw_1d(c1, 4)
+~~~~
+>>> 
+======== RESTART: /home/student/noamgl19_lab5/meet2017y1lab5/fun2.py ========
+>>> draw_1d(c1, 4)
+~~~~
+>>> draw_1d("x", 30)
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+>>> 
